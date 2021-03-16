ccc168a97cd2200cad1586118d0afcbbb3a2581c
Paul OBrien <PVOVideo@outlook.com>
2021-03-16 14:40:14 -0700

    working state

--- tictactoetutorial/src/app.js
+++ tictactoetutorial/src/app.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import Square from './square/square.js'; // always start with "./"
-import Header from './header/header';
+import Header from './header/header'; // always start with "./"
 import Footer from './footer/footer';
 import HornedBeasts from './hornedBeasts/hornedBeasts';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Uniwhal',
+      desc: 'A unicorn and a narwhal nuzzling their horns',
+      image: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
+    }
+  }
+
   render() {
     return (
       <>
         <Header />
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
         <main>
-          <HornedBeasts title={'UniWhal'} desc={'A unicorn and a narwhal nuzzling their horns'} image={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}/>
+          <HornedBeasts title={this.state.title} desc={this.state.desc} image={this.state.image} />
         </main>
         <Footer />
       </>
--- tictactoetutorial/src/hornedBeasts/hornedBeasts.js
+++ tictactoetutorial/src/hornedBeasts/hornedBeasts.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+// assets must be imported up above
 
 class HornedBeasts extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  clickCounter = () => {
+    this.setState({ timesClicked: this.state.timesClicked + 1});
+  }
+
   render() {
     return (
-      <div className="aHornedBeast">
-        <h2>{this.props.title}</h2>
-        <img src={this.props.image}
-          alt={this.props.alt}
-          title={this.props.title}
-          ></img>
-        <p>{this.props.desc}</p>
-      </div>
+      <>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.props.image} />
+        <Card.Body>
+          <Card.Title>{this.props.title} has been clicked {this.state.timesClicked} times</Card.Title>
+          <Card.Text>
+            {this.props.desc}
+          </Card.Text>
+          <Button onClick={this.clickCounter} variant="primary">Click 👍 if you like!</Button>
+        </Card.Body>
+      </Card>
+      </>
+      // <div className="aHornedBeast">
+      //   <h2>{this.props.title}</h2>
+      //   <img src={this.props.image}
+      //     alt={this.props.alt}
+      //     title={this.props.title}
+      //     width={200}
+      //     ></img>
+      //   <p>{this.props.desc}</p>
+      // </div>
     )
   }
 }
