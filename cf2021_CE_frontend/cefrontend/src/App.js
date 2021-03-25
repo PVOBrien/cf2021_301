3fa8661911ae600322005c7b8e8c14b0288bfb40
Paul OBrien <PVOVideo@outlook.com>
2021-03-24 21:21:10 -0700

    save point

--- cf2021_CE_frontend/cefrontend/src/App.js
+++ cf2021_CE_frontend/cefrontend/src/App.js
@@ -1,56 +1,146 @@
 import React from 'react';
 import './App.css';
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Card from 'react-bootstrap/Card';
 
 class Forecast extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       list: [],
+      weatherList: [],
       isListReady: false,
-      isError: false
+      isError: false,
+      location: {},
+      searchQuery: '',
+      imgSrc: '',
+      displayResults: false,
+      error: false,
+      theMessage: ''
     }
   }
 
-  componentDidMount = async () => {
-    const SERVER = 'http://localhost:3001';
+  // componentDidMount = async () => {
+  //   const SERVER = 'http://localhost:3001';
 
+  //   try {
+  //     const theWeather = await axios.get(`${SERVER}/weather`);
+  //     const weatherData = theWeather.data;
+  //     console.log('here', weatherData);
+  //     this.setState({ list: weatherData, isListReady: true });
+  //   } catch (err) {
+  //     console.log('we have an error!');
+  //     this.setState({ isError: true });
+  //   }
+  // }
+
+  getLocationInfo = async (e) => {
+    e.preventDefault();
+    console.log(this.state.searchQuery);
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.searchQuery}&format=json`;
+    const location = await axios.get(url).catch(error => {
+      if (error.response) {
+        this.setState({ error: true });
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        return;
+      } else if (error.request) {
+        console.log(error.request);
+        return;
+      } else {
+        console.log('you survived.')
+      }
+      return;
+    });
+    const locationArray = location.data;
+    this.setState({
+      location: locationArray[0],
+      displayResults: true,
+      imgSrc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13`,
+      error: false
+    });
+    this.getForecastInfo();
+  }
+
+  getForecastInfo = async (e) => {
     try {
-      const theWeather = await axios.get(`${SERVER}/weather`);
-      const weatherData = theWeather.data;
-      console.log('here', weatherData);
-      this.setState({ list: weatherData, isListReady: true });
-    } catch (err) {
-      console.log('we have an error!');
-      this.setState({ isError: true });
+      // e.preventDefault();
+      const SERVER = process.env.REACT_APP_SERVER; // uncertain this is it.
+      const query = { lat: this.state.location.lat, lon: this.state.location.lon };
+      console.log(query);
+      const weather = await axios.get(`${SERVER}/weather/`, { params: query });
+      console.log( 'here');
+      const weatherArr = weather.data;
+      this.setState({ weatherList: weatherArr });
+    } catch (error) {
+      console.log('Error:', error) // what does this do?
     }
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {this.state.isListReady &&
-            <>
-              {this.state.list.map((item, idx) => {
-                return (
-                  <div key={idx} className='weather'>
-                    <p>{item.date}</p>
-                    <p>{item.description}</p>
-                  </div>
-                )
-              })}
-            </>
-          }
-          {this.state.isError &&
-            <>
-              return (
-                <p>We have a problem!</p>
-              )
-            </>
-          }
-        </header>
-      </div >
+      <>
+
+        {this.state.error &&
+          <>
+            <Alert id="alert" variant={'warning'}>What happened?!</Alert>
+          </>
+        }
+
+        <Form onSubmit={this.getLocationInfo}>
+          <Form.Group controlId="formlocation">
+            <Form.Label>Location</Form.Label>
+            <Form.Control onChange={(e) => this.setState({ searchQuery: e.target.value })} type="text" placeholder="Where do you want to go?" />
+            <Form.Text className="text-muted">🎵The road goes ever on and on.🎵</Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit">Submit</Button>
+        </Form>
+
+        {this.state.displayResults &&
+          <>
+            <h2>{this.state.location.display_name}</h2>
+            <Card style={{
+              width: '24rem',
+              padding: '5px',
+              textAlign: 'center'
+            }}>
+              <Card.Img variant="top" src={this.state.imgSrc} />
+              <Card.Body>
+                <Card.Title>{this.state.searchQuery}</Card.Title>
+                {/* <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text> */}
+              </Card.Body>
+            </Card>
+          </>
+        }
+
+        <div className="App">
+          <header className="App-header">
+            {this.state.isListReady ?
+              <>
+                {this.state.list.map((item, idx) => {
+                  return (
+                    <div key={idx} className='weather'>
+                      <p>{item.date}</p>
+                      <p>{item.description}</p>
+                    </div>
+                  )
+                })}
+              </>
+              :
+              <>
+              </>
+            }
+          </header>
+        </div >
+      </>
     );
   }
 }
