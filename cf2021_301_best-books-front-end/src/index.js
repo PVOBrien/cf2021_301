4a1a088a6e50ad23e660ac778596428a75d810c4
Paul OBrien <PVOVideo@outlook.com>
2021-03-29 20:07:35 -0700

    save point

---
+++ cf2021_301_best-books-front-end/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-cf2021-301.us.auth0.com"
+    clientId="iAXLO1ce5r45N1h4Ibcp8F6fjI6mSoIk"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
